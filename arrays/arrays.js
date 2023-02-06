5ca41f092ace3e781b0f42514929ce9664799236
Ladan Nazari <ladannazary@gmail.com>
2023-02-06 17:18:12 +0330

    merge sorted arrays started

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -22,3 +22,31 @@ function reverseString2(str) {
 }
 
 console.log(reverseString2("Hi my name is Andrei"));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 2. Merge Sorted Arrays 
+// We have 2 sorted arrays, we want merge these 2 arrays and create 3rd merged array
+// don't use array methods and solve the problem manually 
+// Questions to ask: 
+// Can one of the arrays be empty? yes!
+// Should the same exact numbers that are repeated in both arrays be added to the 3rd array twice? yes!
+
+function mergeSortedArrays(arr1, arr2) {
+    if (arr1.length === 0) {
+        return arr2
+    } else if (arr2.length === 0) {
+        return arr1
+    }
+    const sortedMerged = [];
+    let ele1 = arr1[0];
+    let ele2 = arr2[0] 
+
+    
+
+
+
+
+}
+
+console.log(mergeSortedArrays([0,3,4,31], [3,4,6,30]));
