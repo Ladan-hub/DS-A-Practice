9f0d441ca60efdd9f8c61bd5b111e660a3bab99c
Ladan Nazari <ladannazary@gmail.com>
2023-02-06 16:48:13 +0330

    first commit

---
+++ arrays/arrays.js
@@ -0,0 +1,15 @@
+// ------ Array Questions ------ //
+
+// 1. Reverse String 
+// "Hi my name is Andrei" => "ierdnA si eman iH"
+
+// - create empty array
+// - iterate through the string backwards 
+// - push each character to the empty array 
+// - return the empty array with the .join("") method
+
+function reverseString (str) {
+    const reversed = [];
+    
+
+}
