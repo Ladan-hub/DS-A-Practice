aa5aa99dc12904e51804ddb3d57713118a17121e
Ladan Nazari <ladannazary@gmail.com>
2023-02-07 17:31:18 +0330

    edge cases

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -33,9 +33,20 @@ console.log(reverseString2("Hi my name is Andrei"));
 // Should the same exact numbers that are repeated in both arrays be added to the 3rd array twice? yes!
 
 function mergeSortedArrays(array1, array2) {
+
+ if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+
     const mergedArray = [];
     let array1Item = array1[0];
     let array2Item = array2[0];
+
+
 }
 
 
