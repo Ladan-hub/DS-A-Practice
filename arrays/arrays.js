a2f29a275a6cfe92d460e2462c4061530a1fe869
Ladan Nazari <ladannazary@gmail.com>
2023-02-07 17:30:44 +0330

    initial indexes

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -32,42 +32,16 @@ console.log(reverseString2("Hi my name is Andrei"));
 // Can one of the arrays be empty? yes!
 // Should the same exact numbers that are repeated in both arrays be added to the 3rd array twice? yes!
 
-// Think about what would happen if one array is shorter than the other array? 
-
-// let's say we want to iterate through two arays and log each pair (you can do a nested loop).
-// What would happen if one array was shorted than the other? 
-
-const arr1 = [1,2,3]
-const arr2 = [1,2,3,4,5,6]
-
-for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++ ) {
-        console.log(arr1[i], "------", arr2[j])
-    }
+function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+    let array1Item = array1[0];
+    let array2Item = array2[0];
 }
 
 
-// function mergeSortedArrays(arr1, arr2) {
-//     if (arr1.length === 0) {
-//         return arr2
-//     } 
-//     if (arr2.length === 0) {
-//         return arr1
-//     }
-//     const sortedMerged = [];
-//     let ele1 = arr1[0];
-//     let ele2 = arr2[0];
-
-//     while(ele1 || ele2) {
-//     if (ele2 === undefined || ele1 < ele2) {
-//         sortedMerged.push(ele1)
-//     }
-//     return sortedMerged;
-
-// }
 
 
 
-// }
+
 
 // console.log(mergeSortedArrays([0,3,4,31], [3,4,6,30]));
