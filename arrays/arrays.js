e11175789eaea9dbf6208da8b830cad78f4c96aa
Ladan Nazari <ladannazary@gmail.com>
2023-02-09 18:02:21 +0330

    edge cases added

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -84,6 +84,15 @@ function mergeSortedArrays(arr1, arr2) {
   let i = 0; 
   let j = 0; 
 
+  // edge cases
+
+  if(arr1.length === 0) {
+        return arr2;
+      }
+  if(arr2.length === 0) {
+        return arr1;
+      }
+
   while(i < arr1.length || j < arr2.length) {
     if (arr2[j] === undefined || arr1[i] < arr2[j] ) {
       result.push(arr1[i]);
