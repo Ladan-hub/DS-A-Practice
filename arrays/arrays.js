f5adc180b674ae791d2af37fe6ead647b6bb4689
Ladan Nazari <ladannazary@gmail.com>
2023-02-12 19:00:57 +0330

    two sum solved with quadratic time complexity

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -132,4 +132,15 @@ Output: [0,1]
 */
 
 
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = 0; j < nums.length; j++) {
+         if (i !== j) {
+                  if (nums[i] + nums[j] === target) {
+                      return [i, j]
+                  }
+              }
 
+          }
+          }
+};
