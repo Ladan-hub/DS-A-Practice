fa1922bcf91c757da6ca242ccf458a539380cb46
Ladan Nazari <ladannazary@gmail.com>
2023-02-13 12:36:57 +0330

    solved

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -176,3 +176,22 @@ var twoSum = function(nums, target) {
   }
   return [];
 };
+
+// Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+var containsDuplicate = function(nums) {
+
+  let map = new Map();
+  
+  for (let i = 0; i < nums.length; i++) {
+    if(map.has(nums[i])) {
+      return true
+    }
+    map.set(nums[i], i)
+  }
+  
+  return false;
+      
+  };
