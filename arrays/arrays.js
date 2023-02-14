6792d9e54802ca5b97cfea71e61d7bfb8757fe87
Ladan Nazari <ladannazary@gmail.com>
2023-02-14 21:48:47 +0330

    solved

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -222,4 +222,16 @@ function addToArray(location, element, arr) {
 
 /* Write a function sumArray(array) that takes in an array of numbers and returns the total sum of all the numbers. */
 
+// KNOW how to find the sum of elements in an array with a counter!
 
+function sumArray(array) {
+  let sum = 0; 
+  for(i = 0; i < array.length; i++) {
+    let el = array[i]; 
+    sum += el; 
+  }
+  return sum; 
+}
+
+console.log(sumArray([5, 6, 4])); // => 15
+console.log(sumArray([7, 3, 9, 11])); // => 30
