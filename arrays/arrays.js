c8ba1a70fd4c038be82ef01fbd75477b4b07c274
Ladan Nazari <ladannazary@gmail.com>
2023-02-15 22:36:15 +0330

    doubler

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -307,3 +307,29 @@ function isSubstring(searchString, subString) {
 
   return lowerSearchStr.indexOf(lowerSubStr) !== -1;
 }
+
+// Doubler
+
+/* Write a function doubler(numbers) that takes an array of numbers and returns a new array
+where every element of the original array is multiplied by 2.
+
+*/
+
+function doubler(numbers) {
+  let doubledNums = [];
+
+  let i = 0;
+  while (i < numbers.length) {
+    let oldNum = numbers[i];
+    let newNum = oldNum * 2;
+    // this step is important because concat does NOT change the original array
+    doubledNums = doubledNums.concat(newNum);
+
+    i += 1;
+  }
+
+  return doubledNums;
+}
+
+console.log(doubler([1, 2, 3, 4])); // => [2, 4, 6, 8]
+console.log(doubler([7, 1, 8])); // => [14, 2, 16]
