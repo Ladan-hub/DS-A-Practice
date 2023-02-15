1fc859efd5400b01f2667aa16cd76ccf2e424fb9
Ladan Nazari <ladannazary@gmail.com>
2023-02-15 22:47:19 +0330

    solved

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -340,3 +340,13 @@ console.log(doubler([7, 1, 8])); // => [14, 2, 16]
 /* Write a function isSubstring that takes in two strings, searchString and subString. The function should return true if subString is a part of thesearchString, regardless of upper or lower case, and false if otherwise. */
 
 
+function isSubstring(searchString, subString) {
+  let lowerSearchStr = searchString.toLowerCase();
+  let lowerSubStr = subString.toLowerCase();
+
+  return lowerSearchStr.indexOf(lowerSubStr) !== -1;
+}
+
+console.log(isSubstring("The cat went to the store", "he cat went")); // => true
+console.log(isSubstring("Time to program", "time")); // => true
+console.log(isSubstring("Jump for joy", "joys")); // => false
