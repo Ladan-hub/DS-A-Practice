2cdb4863cd085e5a80e8718bac5950e0a6b63a03
Ladan Nazari <ladannazary@gmail.com>
2023-02-17 17:03:32 +0330

    solved

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -357,3 +357,21 @@ console.log(isSubstring("Jump for joy", "joys")); // => false
 /* Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 Note that you must do this in-place without making a copy of the array. */
+
+var moveZeroes = function(nums) {
+
+  let i = 0;
+  let j = 0;
+  
+  while(j < nums.length) {
+      if (nums[j] !== 0) {
+          let temp = nums[i]
+          nums[i] = nums[j];
+          nums[j] = temp; 
+
+          i++
+
+      }
+       j++;
+  }
+  };
