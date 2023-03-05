e7708078bc4774c73f7eb6cd8deb3986f3ae039e
Ladan Nazari <ladannazary@gmail.com>
2023-03-05 21:32:07 +0330

    ll 2

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -10,3 +10,24 @@ class ListNode {
   function createLinkedList(val) {
     return new ListNode(val);
   }
+
+
+// Write a function that appends a node to the end of a linked list.
+
+// ADD ONE NODE TO THE END 
+function appendToList(head, value) {
+  
+    let node = new listNode(value)
+    
+    if (!head) {
+      return node
+    }
+    
+    let curr = head;
+    curr.next = node;
+    curr = node
+    return head
+  }
+  
+  // const myList = createLinkedList(3)
+  // console.log(appendToList(myList, 5))
