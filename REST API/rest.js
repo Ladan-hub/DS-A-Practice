3c0f5864eb67b8c860c8682aab1da3b696b8177c
Ladan Nazari <ladannazary@gmail.com>
2023-04-03 12:21:09 -0700

    update user

--- REST API/rest.js
+++ REST API/rest.js
@@ -46,7 +46,6 @@ const connection = mysql.createConnection({
     database:'mydatabase'
 })
 
-
 app.get('/users/:id', (req,res) => {
 
     const id = req.params.id
@@ -65,3 +64,51 @@ app.get('/users/:id', (req,res) => {
     });
 });
 
+// Create a REST API endpoint to add a new user to the "users" table. 
+//The endpoint should accept a JSON object containing the new user's username and email address. 
+//Upon success, the endpoint should return the newly created user object including the user's ID.
+
+app.use(express.json());
+app.post('/users', (req,res) => {
+
+    const { username, email } = req.body;
+
+    const sql = 'INSERT INTO USERS (username,email) VALUES (?,?)';
+
+    connection.query(sql, [username,email], (error,results) => {
+        if (error) return res.status(500).send('Error Retrieving the Data from the Database.');
+
+        if (results.length === 0) {
+            return res.status(404).send('Resource Not Found.')
+        }
+
+        return res.json(results)
+    })
+
+})
+
+
+// Create a REST API endpoint to update an existing user in the "users" table. 
+//The endpoint should accept a JSON object containing the user's ID, username, and email address. 
+//The endpoint should update the corresponding record in the "users" table and return the updated user object.
+
+
+app.use(express.json());
+
+app.put('/users/:id', (req,res) => {
+    const id = req.params.id;
+    const {username, email} = req.body;
+    const sql = 'UPDATE USERS SET USERNAME=?, EMAIL=? WHERE ID=?';
+
+    connection.query(sql, [username,email], (error,results) => {
+        if (error) return res.status(500).send('Error Retrieving the Data from the Database.');
+
+        if (results.length === 0) {
+            return res.status(404).send('Resource Not Found.')
+        }
+
+        return res.json(results)
+    })
+
+
+})
