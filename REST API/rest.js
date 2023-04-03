68a00abf156ebfc96ec7a658f7dbbe8230379eb6
Ladan Nazari <ladannazary@gmail.com>
2023-04-03 12:00:05 -0700

    solution

--- REST API/rest.js
+++ REST API/rest.js
@@ -31,10 +31,37 @@ app.get('/users', (req,res) => {
 });
 
 
-
 // Create a REST API endpoint to retrieve a single user by their ID.
 // The endpoint should accept a parameter for the user ID and return a JSON object containing the user's ID, username, and email address.
 
 
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database:'mydatabase'
+})
+
+
+app.get('/users/:id', (req,res) => {
+
+    const id = req.params.id
+
+    const sql = 'SELECT ID, USERNAME, EMAIL FROM USERS WHERE ID = ?';
+    
+    const connection.query(sql, [id], (error,results) => {
+
+        if (error) return res.status(500).send('Error Retrieving the Data from the Database.');
+
+        if (results.length === 0) {
+            return res.status(404).send('Resource Not Found.')
+        }
+
+        return res.json(results);
+    });
+});
 
