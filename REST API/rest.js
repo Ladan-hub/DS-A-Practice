24647d8dfd8d72974a169f1e6db4547c3741607c
Ladan Nazari <ladannazary@gmail.com>
2023-04-03 11:38:14 -0700

    get users

---
+++ REST API/rest.js
@@ -0,0 +1,5 @@
+// Create a REST API endpoint to retrieve all users from a MySQL database table called "users". The endpoint should return a JSON array of user objects that contain the user's ID, username, and email address.
+
+// app
+
+
