a97474d4746a9cb06811dd473aee58440031255e
Ladan Nazari <ladannazary@gmail.com>
2023-04-05 19:45:39 -0700

    get products and orders

--- REST API/restWithPostgres.js
+++ REST API/restWithPostgres.js
@@ -42,12 +42,12 @@ const connection = new pg.Client({
 
 // Get all users and their orders
 
-app.get('/orders', async (req,res) => {
+app.get('/orders', (req,res) => {
 
     const sql = ' SELECT USERS.*, ORDERS.id, ORDERS.product_id, ORDERS.quantity, ORDERS.order_date FROM USERS INNER JOIN ORDERS ON USERS.id = ORDERS.user_id'; 
 
     // const results = await connection.query(sql);
-    
+
     connection.query(sql, (error, results) => {
 
         if (error) return res.status(500).send('Error Retrieving the Data from the Database');
@@ -57,5 +57,34 @@ app.get('/orders', async (req,res) => {
         return res.json(results);
     })
 })
+
+
+// Get all products and their orders
+
+app.get('/products/orders', async (req,res) => {
+
+    try {
+
+        const sql = 'SELECT PRODUCTS.*, ORDERS.id, ORDERS.user_id, ORDERS.quantity, ORDERS.order_date FROM PRODUCTS INNER JOIN ORDERS on PRODUCTS.id = ORDERS.product_id';
+
+        const results = await connection.query(sql);
+
+        if (results.length === 0) return res.status(404).send('Resource Not Found');
+
+        return res.json(results);
+
+    }
+
+    catch (error) {
+        return res.status(500).send('Error Retrieving the Data from the Database');
+    }
+})
+
+
+// Get all orders for a specific user
+
+
+
+
 
 
