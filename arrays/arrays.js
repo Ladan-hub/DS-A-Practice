b53a36d689b167235020ff065665ca1fedaebf53
Ladan Nazari <ladannazary@gmail.com>
2023-12-18 12:50:27 -0800

    clone an array solution

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -450,3 +450,9 @@ console.log(is_array([1, 2, 4, 0]));
 // console.log(array_Clone([1, 2, [4, 0]]));
 // [1, 2, 4, 0]
 // [1, 2, [4, 0]]
+
+var array_Clone = function(arra1) {
+  return arra1.slice(0);
+     };
+ console.log(array_Clone([1, 2, 4, 0]));
+ console.log(array_Clone([1, 2, [4, 0]]));
