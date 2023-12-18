5cabfb9adb6b969b78cb7cbf96d615619225c5ad
Ladan Nazari <ladannazary@gmail.com>
2023-12-18 12:24:23 -0800

    solution

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -435,3 +435,11 @@ var moveZeroes = function(nums) {
 // console.log(is_array([1, 2, 4, 0]));
 // false
 // true
+
+var is_array = function(input) {
+  if (toString.call(input) === "[object Array]")
+    return true;
+  return false;   
+    };
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
