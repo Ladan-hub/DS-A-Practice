9275ad366954f01a7c4aec6ac003d01e523cb263
Ladan Nazari <ladannazary@gmail.com>
2023-12-19 10:53:11 -0800

    stack

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -55,3 +55,14 @@ function appendToList(head, value) {
 
 // How will you implement stack and queue with JavaScript?
 
+var myStack = [];
+
+//push
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+
+//pop
+myStack.pop(); //3
+myStack.pop(); //2
+myStack.pop(); //1
