584aebb25727c2a600d5e056a880964834eb58d0
Ladan Nazari <ladannazary@gmail.com>
2023-12-22 12:45:49 -0800

    answer

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -165,3 +165,27 @@ console.log("Middle Value Of Linked List is : " + v[middle]);
 
 // Question: How could you write an extension to remove a node from a LL
 
+LinkedList.prototype.remove = function(val){
+  var current = this.head;
+  //case-1
+  if(current.value == val){
+     this.head = current.next;     
+  }
+  else{
+    var previous = current;
+    
+    while(current.next){
+      //case-3
+      if(current.value == val){
+        previous.next = current.next;          
+        break;
+      }
+      previous = current;
+      current = current.next;
+    }
+    //case -2
+    if(current.value == val){
+      previous.next == null;
+    }
+  }
+} 
