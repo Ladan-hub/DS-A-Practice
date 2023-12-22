e357570c6c672fed88ed91ce239671c4ac2f53a6
Ladan Nazari <ladannazary@gmail.com>
2023-12-22 12:37:13 -0800

    find the middle of the linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -122,4 +122,7 @@ sll.head; //Object {data: 2, next: Object}
 sll.head.next; //Object {data: 3, next: Object}
 sll.head.next.next; //Object {data: 4, next: null}
         
+//Given a singly linked list, find the middle of the linked list. For example, if the given linked list is 1->2->3->4->5 then the output should be 3. 
+// If there are even nodes, then there would be two middle nodes, we need to print the second middle element. 
+// For example, if the given linked list is 1->2->3->4->5->6 then the output should be 4. 
 
