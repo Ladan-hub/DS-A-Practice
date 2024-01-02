430411a9cd99366cdaba7406d76bee4fe1e88c8c
Ladan Nazari <ladannazary@gmail.com>
2024-01-02 13:27:44 -0800

    solution

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -497,3 +497,13 @@ console.log(arrayOfOddNumbers.length);
 
 const arrayOfNumbers = [1, 2, 3, 4];
 arrayOfNumbers.reduce((accumulator, currentValue, index, array) => array[index] = array[index] * 2);
+
+
+// What is the output of the following code snippet?
+
+let arrayOfLetters = ['a','b','c','d','e','f'];
+const anotherArrayOfLetters = arrayOfLetters;
+arrayOfLetters = [];
+console.log(anotherArrayOfLetters);
+
+['a','b','c','d','e','f']
