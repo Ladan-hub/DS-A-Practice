9458d67880b0218af069491d82eec1e2e304e0fb
Ladan Nazari <ladannazary@gmail.com>
2024-01-02 13:17:05 -0800

    reason for solution

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -487,3 +487,6 @@ arrayOfOddNumbers[100] = 199;
 console.log(arrayOfOddNumbers.length);
 
 101
+
+
+// The reason for this solution is as follows: JavaScript placesempty as a value for indices 3 - 99. Thus, when you set the value of the 100th index, the array looks like:
