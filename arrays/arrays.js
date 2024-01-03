e2fe4c3c93998a308fee25fed4fade00e001d8cd
Ladan Nazari <ladannazary@gmail.com>
2024-01-03 15:06:23 -0800

    solution

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -514,4 +514,9 @@ console.log(anotherArrayOfLetters);
 // const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
 // console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
 
+function getUniqueValues(arrOfNum) {
+  const set = new Set(arrOfNum);
+  return [...set];
+}
+
 
