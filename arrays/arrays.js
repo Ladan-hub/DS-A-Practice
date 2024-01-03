57008c0db6bfec3164d7f9b24ab48226997c4dc7
Ladan Nazari <ladannazary@gmail.com>
2024-01-03 14:56:31 -0800

    unique values from array

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -507,3 +507,11 @@ arrayOfLetters = [];
 console.log(anotherArrayOfLetters);
 
 ['a','b','c','d','e','f']
+
+
+// How could you print unique values from an array?
+
+// const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+// console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+
+
