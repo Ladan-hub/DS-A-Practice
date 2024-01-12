744cb5b8d5e993f4422d09de4af9f4387cc577cc
Ladan Nazari <ladannazary@gmail.com>
2024-01-11 20:46:23 -0800

    equality operators

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -757,3 +757,11 @@ var x;
 
 
 // The debugger for the browser must be activated in order to debug the code. Built-in debuggers may be switched on and off, requiring the user to report faults. The remaining section of the code should stop execution before moving on to the next line while debugging.
+
+//4. Difference between “ == “ and “ === “ operators.
+
+var x = 2;
+var y = "2";
+(x == y)  // Returns true since the value of both x and y is the same
+(x === y) // Returns false since the typeof x is "number" and typeof y is "string"
+
