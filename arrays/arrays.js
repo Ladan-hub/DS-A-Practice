3f1ddbb1a2b7f8c56d045edc7d7bb365b27c96bc
Ladan Nazari <ladannazary@gmail.com>
2024-01-12 20:52:47 -0800

    difference between let and var

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -765,3 +765,11 @@ var y = "2";
 (x == y)  // Returns true since the value of both x and y is the same
 (x === y) // Returns false since the typeof x is "number" and typeof y is "string"
 
+
+//Difference between var and let keyword in javascript.
+
+// Some differences are 
+
+// From the very beginning, the 'var' keyword was used in JavaScript programming whereas the keyword 'let' was just added in 2015.
+// The keyword 'Var' has a function scope. Anywhere in the function, the variable specified using var is accessible but in ‘let’ the scope of a variable declared with the 'let' keyword is limited to the block in which it is declared. Let's start with a Block Scope.
+// In ECMAScript 2015, let and const are hoisted but not initialized. Referencing the variable in the block before the variable declaration results in a ReferenceError because the variable is in a "temporal dead zone" from the start of the block until the declaration is processed.
