066281741af7c1ea29a2b244654aecfceb6bdebb
Ladan Nazari <ladannazary@gmail.com>
2024-01-12 21:22:55 -0800

    higher order function

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -851,3 +851,24 @@ function() {
 // Engineers will not be allowed to create global variables in 'Strict Mode.
 
 
+//Explain Higher Order Functions in javascript.
+
+Functions that operate on other functions, either by taking them as arguments or by returning them, are called higher-order functions.
+
+Higher-order functions are a result of functions being first-class citizens in javascript.
+
+Examples of higher-order functions:
+
+function higherOrder(fn) {
+  fn();
+}
+   
+higherOrder(function() { console.log("Hello world") });  
+function higherOrder2() {
+  return function() {
+    return "Do something";
+  }
+}      
+var x = higherOrder2();
+x()   // Returns "Do something"
+
