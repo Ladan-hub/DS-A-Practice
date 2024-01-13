06d3dfd1bca93c44b7c7d032d8dbe78e3a6385ac
Ladan Nazari <ladannazary@gmail.com>
2024-01-12 21:15:23 -0800

    Immediately Invoked Function

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -776,11 +776,11 @@ var y = "2";
 
 // What is NaN property in JavaScript?
 
-NaN property represents the “Not-a-Number” value. It indicates a value that is not a legal number.
+//NaN property represents the “Not-a-Number” value. It indicates a value that is not a legal number.
 
-typeof of NaN will return a Number.
+//typeof of NaN will return a Number.
 
-To check if a value is NaN, we use the isNaN() function,
+//To check if a value is NaN, we use the isNaN() function,
 
 Note- isNaN() function converts the given value to a Number type, and then equates to NaN.
 isNaN("Hello")  // Returns true
@@ -799,3 +799,44 @@ isNaN(undefined) // Returns true
 
 // var x = 2;
 // In the above example, we created a variable x and assigned it a value of “2”. In the background, the “=” (assign operator) allocates some space in the memory, stores the value “2” and returns the location of the allocated memory space. Therefore, the variable x in the above code points to the location of the memory space instead of pointing to the value 2 directly.
+
+
+//What is an Immediately Invoked Function in JavaScript?
+
+//An Immediately Invoked Function ( known as IIFE and pronounced as IIFY) is a function that runs as soon as it is defined.
+
+Syntax of IIFE :
+
+(function(){ 
+  // Do something;
+})();
+//To understand IIFE, we need to understand the two sets of parentheses that are added while creating an IIFE :
+
+//The first set of parenthesis:
+
+(function (){
+   //Do something;
+})
+////While executing javascript code, whenever the compiler sees the word “function”, it assumes that we are declaring a function in the code. Therefore, if we do not use the first set of parentheses, the compiler throws an error because it thinks we are declaring a function, and by the syntax of declaring a function, a function should always have a name.
+
+function() {
+  //Do something;
+}
+// Compiler gives an error since the syntax of declaring a function is wrong in the code above.
+//To remove this error, we add the first set of parenthesis that tells the compiler that the function is not a function declaration, instead, it’s a function expression.
+
+//The second set of parenthesis:
+
+(function (){
+  //Do something;
+})();
+//From the definition of an IIFE, we know that our code should run as soon as it is defined. A function runs only when it is invoked. If we do not invoke the function, the function declaration is returned:
+
+(function (){
+  // Do something;
+})
+
+// Returns the function declaration
+//Therefore to invoke the function, we use the second set of parenthesis.
+
+
