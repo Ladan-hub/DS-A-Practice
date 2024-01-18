a81caa11595046edbcf45b674284296c6a8b5bed
Ladan Nazari <ladannazary@gmail.com>
2024-01-17 16:28:05 -0800

    call()

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -962,3 +962,15 @@ var obj = {name: "Sandy"};
 sayHello.call(obj);
         
 // Returns "Hello Sandy"	
+
+// example 2:
+
+var person = {
+  age: 23,
+  getAge: function(){
+    return this.age;
+  }
+}        
+var person2 = {age:  54};
+person.getAge.call(person2);      
+// Returns 54  
