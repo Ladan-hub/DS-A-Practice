8fa99da9238ab3b8c87eb485473738d769b9016e
Ladan Nazari <ladannazary@gmail.com>
2024-01-18 13:08:37 -0800

    advantages of using external javascript

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1049,3 +1049,15 @@ var curriedMultiply = currying(multiply);
 multiply(4, 3); // Returns 12
 
 curriedMultiply(4)(3); // Also returns 12
+
+
+// What are some advantages of using External JavaScript?
+
+// External JavaScript is the JavaScript Code (script) written in a separate file with the extension.js, and then we link that file inside the <head> or <body> element of the HTML file where the code is to be placed. 
+
+// Some advantages of external javascript are
+
+// It allows web designers and developers to collaborate on HTML and javascript files.
+// We can reuse the code.
+// Code readability is simple in external javascript.
+
