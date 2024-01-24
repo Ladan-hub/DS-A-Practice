38f0ed3a3a4d4a54caec522e829c54ac7d48ec95
Ladan Nazari <ladannazary@gmail.com>
2024-01-23 21:20:36 -0800

    object prototype

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1149,3 +1149,20 @@ function randomFunc(){
 var initialiseClosure = randomFunc(); // Returns a function
 
 initialiseClosure(); 
+
+
+// What are object prototypes?
+
+// All javascript objects inherit properties from a prototype. For example,
+
+// Date objects inherit properties from the Date prototype
+// Math objects inherit properties from the Math prototype
+// Array objects inherit properties from the Array prototype.
+// On top of the chain is Object.prototype. Every prototype inherits properties and methods from the Object.prototype.
+// A prototype is a blueprint of an object. The prototype allows us to use properties and methods on an object even if the properties and methods do not exist on the current object.
+// Let’s see prototypes help us use methods and properties:
+
+var arr = [];
+arr.push(2);
+
+console.log(arr); // Outputs [2]
