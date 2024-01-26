deab93c4104341ac78ee9f09ffe4f540eb970972
Ladan Nazari <ladannazary@gmail.com>
2024-01-25 20:53:47 -0800

    memoization

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1203,3 +1203,29 @@ operationOnSum(5, 5, multiplyBy2); // Outputs 20
 // Syntax error: Syntax errors are mistakes or spelling problems in the code that cause the program to not execute at all or to stop running halfway through. Error messages are usually supplied as well.
 // Logical error: Reasoning mistakes occur when the syntax is proper but the logic or program is incorrect. The application executes without problems in this case. However, the output findings are inaccurate. These are sometimes more difficult to correct than syntax issues since these applications do not display error signals for logic faults.
 
+
+// What is memoization?
+// Memoization is a form of caching where the return value of a function is cached based on its parameters. If the parameter of that function is not changed, the cached version of the function is returned.
+// Let’s understand memoization, by converting a simple function to a memoized function:
+
+// Note- Memoization is used for expensive function calls but in the following example, we are considering a simple function for understanding the concept of memoization better.
+
+
+function memoizedAddTo256(){
+  var cache = {};
+
+  return function(num){
+    if(num in cache){
+      console.log("cached value");
+      return cache[num]
+    }
+    else{
+      cache[num] = num + 256;
+      return cache[num];
+    }
+  }
+}
+var memoizedFunc = memoizedAddTo256();
+
+memoizedFunc(20); // Normal return
+memoizedFunc(20); // Cached return
