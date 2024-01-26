1dbee71742129f5718d0ddc93a8b9f73facaf838
Ladan Nazari <ladannazary@gmail.com>
2024-01-25 21:03:52 -0800

    recursion

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1229,3 +1229,31 @@ var memoizedFunc = memoizedAddTo256();
 
 memoizedFunc(20); // Normal return
 memoizedFunc(20); // Cached return
+
+// What is recursion in a programming language?
+
+function add(number) {
+  if (number <= 0) {
+    return 0;
+  } else {
+    return number + add(number - 1);
+  }
+}
+add(3) => 3 + add(2)
+          3 + 2 + add(1)
+          3 + 2 + 1 + add(0)
+          3 + 2 + 1 + 0 = 6  
+
+
+//The following function calculates the sum of all the elements in an array by using recursion:
+
+function computeSum(arr){
+  if(arr.length === 1){
+    return arr[0];
+  }
+  else{
+    return arr.pop() + computeSum(arr);
+  }
+}
+computeSum([7, 8, 9, 99]); // Returns 123
+
