d891c76c0dfc8b1d41962213a688adad6315855f
Ladan Nazari <ladannazary@gmail.com>
2024-01-30 13:02:48 -0800

    slution

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1360,3 +1360,27 @@ function varVsLetFunction(){
 
 console.log(awesomeCar1); // Throws an error
 console.log(awesomeCar2); // Throws an error
+
+{
+  var variable3 = [1, 2, 3, 4];
+}
+
+console.log(variable3); // Outputs [1,2,3,4]
+
+{
+  let variable4 = [6, 55, -1, 2];
+}
+
+console.log(variable4); // Throws error
+
+for(let i = 0; i < 2; i++){
+  //Do something
+}
+
+console.log(i); // Throws error
+
+for(var j = 0; j < 2; i++){
+  // Do something
+}
+
+console.log(j) // Outputs 2 
