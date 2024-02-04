0effdbc93afe43875568f614bf7bc8894bdd40d6
Ladan Nazari <ladannazary@gmail.com>
2024-02-03 20:23:53 -0800

    promises explanation

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1472,3 +1472,28 @@ console.log(mergedObj);
 
 // Rest parameter is used to take a variable number of arguments and turns them into an array while the spread operator takes an array or an object and spreads it
 // Rest parameter is used in function declaration whereas the spread operator is used in function calls.
+
+
+// What is the use of promises in javascript?
+
+// Before promises, callbacks were used to handle asynchronous operations. But due to the limited functionality of callbacks, using multiple callbacks to handle asynchronous code can lead to unmanageable code.
+
+// Promise object has four states -
+
+// Pending - Initial state of promise. This state represents that the promise has neither been fulfilled nor been rejected, it is in the pending state.
+// Fulfilled - This state represents that the promise has been fulfilled, meaning the async operation is completed.
+// Rejected - This state represents that the promise has been rejected for some reason, meaning the async operation has failed.
+// Settled - This state represents that the promise has been either rejected or fulfilled.
+// A promise is created using the Promise constructor which takes in a callback function with two parameters, resolve and reject respectively.
+
+
+// resolve is a function that will be called when the async operation has been successfully completed.
+
+// reject is a function that will be called, when the async operation fails or if some error occurs.
+
+// Example of a promise:
+
+// Promises are used to handle asynchronous operations like server requests, for ease of understanding, we are using an operation to calculate the sum of three elements.
+
+
+
