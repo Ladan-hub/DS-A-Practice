13c38a51598fc75a17cad80ef436f47f3cb1db5c
Ladan Nazari <ladannazary@gmail.com>
2024-02-03 20:24:20 -0800

    promises code example

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1496,4 +1496,21 @@ console.log(mergedObj);
 // Promises are used to handle asynchronous operations like server requests, for ease of understanding, we are using an operation to calculate the sum of three elements.
 
 
+function sumOfThreeElements(...elements){
+  return new Promise((resolve,reject)=>{
+    if(elements.length > 3 ){
+      reject("Only three elements or less are allowed");
+    }
+    else{
+      let sum = 0;
+      let i = 0;
+      while(i < elements.length){
+        sum += elements[i];
+        i++;
+      }
+      resolve("Sum has been calculated: "+sum);
+    }
+  })
+}
+
 
