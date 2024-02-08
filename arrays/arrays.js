869ff5d8e15ec1173573dd35e5dadeeed84d1967
Ladan Nazari <ladannazary@gmail.com>
2024-02-07 20:33:03 -0800

    primitive data types

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1719,4 +1719,12 @@ function anotherRandomFunc(){
 anotherRandomFunc();
 // In the code above, both in the global scope and functional scope, we are trying to access variables that have not been declared yet. This is called the Temporal Dead Zone.
 
+// What are the primitive data types in JavaScript?
+// A primitive is a data type that isn't composed of other data types. It's only capable of displaying one value at a time. By definition, every primitive is a built-in data type (the compiler must be knowledgeable of them) nevertheless, not all built-in datasets are primitives. In JavaScript, there are 5 different forms of basic data. The following values are available:
+
+// Boolean
+// Undefined
+// Null
+// Number
+// String
 
