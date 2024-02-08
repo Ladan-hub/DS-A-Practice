82ac8a87e7933e4a4a3059a40b29cfc9aa5be231
Ladan Nazari <ladannazary@gmail.com>
2024-02-07 19:40:22 -0800

    object destructuring

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1677,3 +1677,22 @@ console.log(classBenches); // Outputs 39
 console.log(classBlackBoard); // Outputs 1
 
 
+//As one can see, using object destructuring we have extracted all the elements inside an object in one line of code. If we want our new variable to have the same name as the property of an object we can remove the colon:
+
+const {strength:strength} = classDetails;
+// The above line of code can be written as:
+const {strength} = classDetails;
+Array destructuring: Before ES6 version:
+const arr = [1, 2, 3, 4];
+const first = arr[0];
+const second = arr[1];
+const third = arr[2];
+const fourth = arr[3];
+The same example using object destructuring:
+
+const arr = [1, 2, 3, 4];
+const [first,second,third,fourth] = arr;
+console.log(first); // Outputs 1
+console.log(second); // Outputs 2
+console.log(third); // Outputs 3
+console.log(fourth); // Outputs 4
