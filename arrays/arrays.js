c9e9021502ad735000703cc54e57b52f64bb19b9
Ladan Nazari <ladannazary@gmail.com>
2024-02-13 21:17:13 -0800

    lexical scoping

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1733,4 +1733,7 @@ anotherRandomFunc();
 
 // The processing of HTML code while the page loads are disabled by nature till the script hasn't halted. Your page will be affected if your network is a bit slow, or if the script is very hefty. When you use Deferred, the script waits for the HTML parser to finish before executing it. This reduces the time it takes for web pages to load, allowing them to appear more quickly.
 
+// What has to be done in order to put Lexical Scoping into practice?
+// To support lexical scoping, a JavaScript function object's internal state must include not just the function's code but also a reference to the current scope chain.
+
 
