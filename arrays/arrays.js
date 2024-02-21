6336c39b2ce6a697ebcef3a23d2e9bed438a8126
Ladan Nazari <ladannazary@gmail.com>
2024-02-20 21:10:01 -0800

    two strings are anagrams of one another

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -2076,3 +2076,23 @@ rotateRight([44, 1, 22, 111], 5); // Returns [111,44,1,22]
 <body> <p id="parent1">firstP<p> </body> 
 </html>
 
+
+// Write the code given If two strings are anagrams of one another, then return true.
+
+var firstWord = "Deepak";
+var secondWord = "Aman";
+
+isAnagram(wordOne, wordTwo); // true
+
+function isAnagram(one, two) {
+  //Change both words to lowercase for case insensitivity..
+  var a = one.toLowerCase();
+  var b = two.toLowerCase();
+
+  // Sort the strings, then combine the array to a string. Examine the outcomes.
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
+
