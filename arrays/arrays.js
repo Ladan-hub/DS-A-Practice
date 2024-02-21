b04bec5886733218b7d86b8d42411ff57dfb3274
Ladan Nazari <ladannazary@gmail.com>
2024-02-20 21:11:35 -0800

    find vowels

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -2096,3 +2096,16 @@ function isAnagram(one, two) {
   return a === b;
 }
 
+
+// Write the code to find the vowels
+
+const findVowels = str => {
+  let count = 0
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  for(let char of str.toLowerCase()) {
+    if(vowels.includes(char)) {
+      count++
+    }
+  }
+  return count
+}
