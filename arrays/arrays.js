82959d1211c13cab12947abdb500870ffdf13c1f
Ladan Nazari <ladannazary@gmail.com>
2024-02-22 13:13:46 -0800

    answ

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -2138,3 +2138,23 @@ for (var i = 0; i < 10; i++) {
   setTimeout(() => console.log(b[i]), 1000);
 }
 
+{/* 1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+undefined
+undefined
+undefined
+undefined
+undefined
+undefined
+undefined
+undefined
+undefined
+undefined */}
